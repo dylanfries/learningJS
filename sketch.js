4a64efc2c9494b26b8ff0bd75e48c3bc62a33ac3
Dylan Fries <dylan.fries@connect.7oaks.org>
2021-08-31 13:14:05 -0500

    Set canvas translate to center so the heart is based at 0,0 (with some offset from its initial settings), also added a sine wave that changes the size of the heart.

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 
 // define the heart object
 let heart = {
-    baseY : 150,
-    baseX : 200,
+    baseY : -50,
+    baseX : 0,
     circleDiam : 100,
     circleRadius : 50,
     triangleHeight : 150,
@@ -10,21 +10,41 @@ let heart = {
     color : 0,
   }
 
-
+let currentScale = 1;
+let timer = 0;
+let sineFrequency = 1/500;
+let sineAmplitude = 1/2;
 
 function setup() {
   // 400 px x 400px
   createCanvas(400, 400);
   
+
   console.log(heart);
   
 }
 
 function draw() {
-  //background(220);
+  
+  background(220);
 
   fill(heart.color);
 
+    // set the origin to the center. 
+  translate(width/2, height/2);
+
+  // timing 
+  timer += deltaTime;
+
+  // make it get bigger and smaller
+  let y = sineAmplitude * sin(timer * sineFrequency);
+  console.log(timer + " " + y);
+  scale(currentScale + y);
+
+
+
+// draw the heart, because it is using the base already, it should work with the
+// translate if we modify baseX and baseY values to compensate
   circle(heart.baseX + heart.circleRadius, heart.baseY, heart.circleDiam);
   circle(heart.baseX - heart.circleRadius, heart.baseY, heart.circleDiam);
   triangle(heart.baseX + heart.circleDiam, heart.baseY + heart.triangleFudge,
