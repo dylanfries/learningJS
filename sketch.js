120d4a4d2835424ccb1e9143c90fc4e7b513ac2d
Dylan Fries <dyl@dylanfries.com>
2021-08-31 13:52:34 -0500

    Some basic mouse interaction. Having some weird effects with the canvas, good learning opportunity.

--- sketch.js
+++ sketch.js
@@ -29,9 +29,12 @@ function setup() {
 
 function draw() {
   
+  // timing, deltaTime is the time since the last frame or the inverse of the framerate 
+  timer += deltaTime;
+
   // make it get bigger and smaller
-  let y = sineAmplitude * sin(timer * sineFrequency);
-  console.log(timer + " " + y);
+  let sineY = sineAmplitude * sin(timer * sineFrequency);
+  console.log(timer + " " + sineY);
 
   // lets make the background fancier
 
@@ -46,14 +49,11 @@ function draw() {
 
   fill(heart.color);
 
+  //push();
     // set the origin to the center. 
   translate(width/2, height/2);
-
-  // timing 
-  timer += deltaTime;
-
-
-  scale(currentScale + y);
+  scale(currentScale + sineY);
+  //pop(); // ?? 
 
 // draw the heart, because it is using the base already, it should work with the
 // translate if we modify baseX and baseY values to compensate
@@ -63,9 +63,30 @@ function draw() {
       heart.baseX - heart.circleDiam, heart.baseY + heart.triangleFudge,
       heart.baseX, heart.baseY + heart.triangleHeight);
 
-/*
+/* Original version
   circle(150,150,100);
   circle(250,150,100);
   triangle(100,158, 300,158, 200, 300);
 */
+  
+  // Input
+  // https://stungeye.github.io/Applied-Math-For-Games-1/docs/04-introduction-to-processing/03-user-input.html
+  let xPos = constrain(mouseX, 0, width);
+  let yPos = constrain(mouseY, 0, height);
+
+
+  // manually reverse the orientation settings
+  translate(-width/2, -height/2);
+  //scale(currentScale + sineY);// prbably fine
+
+  // Not working yet but pretty cool anyways
+  // map function
+  let circleSize = 50;
+  // map 0-width to 0-255
+  let red = map(xPos, 0, width, 255);
+  let green = map(yPos, 0, height, 255);
+  fill(255, 0, 0);
+  circle(xPos, yPos, sineY*circleSize);
+
+
 }
